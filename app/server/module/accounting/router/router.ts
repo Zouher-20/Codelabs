67ec0a041049c77dfa2e3c643a4cfa0ac827fd72
AbdAlrahman <abd233661@gmail.com>
2024-03-06 15:53:34 +0300

    create-prisma-ORM

--- app/server/module/accounting/router/router.ts
+++
@@ -1,7 +0,0 @@
-import express from 'express';
-
-const router = express.Router();
-
-router.post('/register/admin');
-
-export default router;
--- app/server/router/router.ts
+++
@@ -1,8 +0,0 @@
-import express from 'express';
-import Accounting from '../module/accounting/router/router';
-
-const router = express.Router();
-
-router.use('/accounting', Accounting);
-
-export default router;
