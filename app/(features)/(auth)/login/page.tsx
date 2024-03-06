7c3658ffb620f23594af9aef2c45f4e57f93f6d3
Zouhair Nasser <zouhair.nasser@workiom.com>
2024-03-07 02:12:04 +0300

    Initialized frontend structure

---
+++ app/(features)/(auth)/login/page.tsx
@@ -0,0 +1,10 @@
+import AuthCardComponent from '../components/auth-card';
+
+export default function LoginPage() {
+    return (
+        <div className="flex flex-col  p-24 text-4xl">
+            <AuthCardComponent />
+            Hello Auth
+        </div>
+    );
+}
---
+++ app/(features)/(sample)/layout.tsx
@@ -0,0 +1,7 @@
+export default function SampleLayout({
+    children
+}: Readonly<{
+    children: React.ReactNode;
+}>) {
+    return <div>{children}</div>;
+}
