dc99f3e97b99cd1dc77c54310967f9639e7b71ff
aymanm <099aymam@gmail.com>
2024-03-15 14:07:24 +0300

    #

--- app/components/globals/form/button/index.tsx
+++ app/components/globals/form/button/index.tsx
@@ -1,4 +1,4 @@
-const Button = ({ lable, style, onClick }: { lable?: string, style?: string, onClick?: () => void }) => {
+const Button = ({ label, style, onClick }: { label?: string, style?: string, onClick?: () => void }) => {
     const basic = ' bg-white text-black hover:bg-base-100 hover:text-white';
     const outlin = 'bg-base-300 text-white border-1 border-white hover:bg-white hover:text-black';
     const fill = 'bg-base-100';
@@ -8,7 +8,7 @@ const Button = ({ lable, style, onClick }: { lable?: string, style?: string, onC
                 +
                 (style == 'basic' ? basic : (style == 'fill' ? fill : (style == 'outline' ? outlin : 'bg-base-200 text-primary')))}
         >
-            {lable}
+            {label}
         </button >
     );
 }
--- app/page.tsx
+++ app/page.tsx
@@ -12,10 +12,10 @@ export default function Home() {
     return <PageContainer >
         <div className='p-8 grid gap-4'>
             <div className='flex gap-4'>
-                <Button onClick={HandleClick} lable='basic' style='basic' />
-                <Button onClick={HandleClick} lable='outline' style='outline' />
-                <Button onClick={HandleClick} lable='fill' style='fill' />
-                <Button onClick={HandleClick} lable='any' style='any' />
+                <Button onClick={HandleClick} label='basic' style='basic' />
+                <Button onClick={HandleClick} label='outline' style='outline' />
+                <Button onClick={HandleClick} label='fill' style='fill' />
+                <Button onClick={HandleClick} label='any' style='any' />
             </div>
         </div>
     </PageContainer>;
