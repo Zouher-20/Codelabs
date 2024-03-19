7894f630deef3d5b24d63b3fc9ab6221f205d7a1
aymanm <099aymam@gmail.com>
2024-03-19 14:42:42 +0300

    user-plan-component and some clean code

---
+++ app/components/cards/plans.tsx
@@ -0,0 +1,39 @@
+export const basicPlan = {
+    title: 'Basic',
+    subtitle: 'Perfect plan for Starters.',
+    price: 'Free',
+    duration: 'For a life',
+    advantages: [
+        'Limited 5 labs',
+        'Limited to 2 classes',
+        'Only 5 students in the class',
+        'Only 2 labs in the class',
+        'Participate in challenges'
+    ]
+}
+export const plusPlan = {
+    title: 'plus',
+    subtitle: 'For users who want to do more.',
+    price: '$5',
+    duration: '/Year',
+    advantages: [
+        'Limited 10 labs',
+        'Limited to 5 classes',
+        'Only 10 students in the class',
+        'Only 5 labs in the class',
+        'Participate in challenges'
+    ]
+}
+export const premiumPlan = {
+    title: 'premium',
+    subtitle: 'For users who are seeking for the best',
+    price: '$40',
+    duration: '/Year',
+    advantages: [
+        'Unlimited labs',
+        'Unlimited classes',
+        'Up to 25 students in the class',
+        'Up to 25 labs in the class',
+        'Participate in challenges'
+    ]
+}
---
+++ app/components/cards/user-plan-card/index.tsx
@@ -0,0 +1,31 @@
+import IconRenderer from "../../globals/icon";
+
+type Props = {
+    plan: {
+        title: string,
+        subtitle: string,
+        price: string,
+        duration: string,
+    }
+    active?: boolean
+}
+const UserPlanCard = ({ plan, active }: Props) => {
+    const { title, subtitle, price, duration } = plan
+    return (
+        <div className={"py-4 px-6 max-w-xs flex flex-col gap-4  rounded-3xl border-base-100 border-2 " + (active ? 'bg-base-100' : '')}>
+            <div className="flex justify-between ">
+                <IconRenderer className={active ? 'text-white' : 'text-primary'} fontSize={33} icon="solar:link-circle-line-duotone" />
+                <IconRenderer className={"self-center hover:opacity-65  " + (active ? 'text-white' : 'text-primary')} fontSize={28} icon="solar:pen-2-bold-duotone" />
+            </div>
+            <div className="grid">
+                <p className="text-2xl text-white">{title}</p>
+                <p className="text-gray-400">{subtitle}</p>
+            </div>
+            <div className="grid gap-1">
+                <p className={"text-4xl font-bold " + (active ? 'text-white' : 'text-primary')}>{price}</p>
+                <p>{duration}</p>
+            </div>
+        </div>
+    );
+}
+export default UserPlanCard;
