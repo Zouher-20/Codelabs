cad79e624f0624354d8da8cb60a4a9851656f083
aymanm <099aymam@gmail.com>
2024-03-25 17:02:22 +0300

    challenge-card fixed

--- app/components/cards/challenge/index.tsx
+++ app/components/cards/challenge/index.tsx
@@ -1,19 +1,23 @@
+import Image from "next/image";
 import Button from "../../globals/form/button";
-import Lab from "../../globals/lab/lab";
+import defaultImage from '../../../../public/lab.png'
 
-const Challenge = ({ name, description, onClick }: {
+const Challenge = ({ image, name, description, onClick }: {
+    image?: string,
     name: string,
     description: string,
     onClick?: () => void
 }) => {
     return (
-        <div className="w-3/4 bg-base-100 flex flex-col gap-1 p-4 rounded-2xl">
+        <div className=" bg-base-100 flex flex-col gap-1 p-4 rounded-2xl">
             <div className="flex gap-8">
-                <Lab />
-                <div className="flex flex-col gap-4 slef-center w-full">
+                <div className="min-w-64  h-min pt-12 bg-base-200 flex flex-col items-center rounded-xl">
+                    <Image className="h-28 mb-8 w-32" src={image ? image : defaultImage} alt="image" />
+                </div>
+                <div className="flex flex-col gap-2 slef-center w-full py-4">
                     <h1 className="text-2xl font-bold">{name}</h1>
                     <p className=" tracking-wide h-full">{description}</p>
-                    <section className="self-end"><Button label="enroll" color="any" onClick={onClick} /></section>
+                    <section ><Button label="enroll" style="any" onClick={onClick} /></section>
                 </div>
             </div>
         </div>
