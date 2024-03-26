bbd6401998cf90a6a1eaff3b4a152c1e14a4fa72
mhd.yazbek <mhd.yazbek@yolo.clinic>
2024-03-26 16:53:08 +0200

    commit

--- app/(features)/(main)/discover/components/lab-list-component.tsx
+++ app/(features)/(main)/discover/components/lab-list-component.tsx
@@ -20,9 +20,9 @@ export default function LabListComponent({
             <div className="h-3"></div>
             <div className="carousel relative w-full rounded-box">
                 <div className="carousel-item">
-                    {labs.map(e => (
-                        <div className="px-1">
-                            <LabCard key={e.title} title={e.title ?? ''} />
+                    {labs.map((e, index) => (
+                        <div className="px-1" key={e.title + `${index}`}>
+                            <LabCard title={e.title ?? ''} />
                         </div>
                     ))}
                 </div>
