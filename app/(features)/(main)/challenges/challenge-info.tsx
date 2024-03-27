432fde361943c536ae728e768b68371a1cfe2640
aymanm <099aymam@gmail.com>
2024-03-27 17:37:02 +0300

    challenges view with empty state - UI 

---
+++ app/(features)/(main)/challenges/challenge-info.tsx
@@ -0,0 +1,22 @@
+import IconRenderer from "@/app/components/globals/icon";
+
+interface Props {
+    items: {
+        icon: string;
+        label: string;
+        color: string;
+    }[]
+}
+const ChallengeInfo = ({ items }: Props) => {
+    return (
+        <div className='justify-center p-8 bg-base-100 flex flex-col gap-4 h-full rounded-2xl'>
+            {items.map((item) => (
+                <section className='flex gap-3'>
+                    <IconRenderer className={'self-center ' + item.color} fontSize={24} icon={item.icon} />
+                    <p>{item.label}</p>
+                </section>
+            ))}
+        </div>
+    )
+}
+export default ChallengeInfo;
--- app/(features)/(main)/challenges/page.tsx
+++ app/(features)/(main)/challenges/page.tsx
@@ -1,5 +1,59 @@
 'use client';
+import Image from 'next/image';
+import imageEmpty from '@/public/background/challenge-background.png'
+import Challenge from '@/app/components/cards/challenge';
+import IconRenderer from '@/app/components/globals/icon';
+import TournamentCard from './tournament-card';
+import ChallengeInfo from './challenge-info';
+
+const challenges = true;
+
+const items = [{
+    icon: 'solar:bookmark-circle-broken',
+    label: 'type',
+    color: 'text-info'
+},
+{
+    icon: 'solar:bonfire-line-duotone',
+    label: 'Difficulty : hard',
+    color: 'text-error'
+},
+{
+    icon: 'solar:clock-square-bold-duotone',
+    label: 'start in : 1h 30 sec',
+    color: 'text-secondary'
+},
+{
+    icon: 'solar:virus-bold-duotone',
+    label: 'already enrolled : 30  ',
+    color: 'text-primary'
+}
+]
 
 export default function LabsPage() {
-    return <div className="flex flex-col  p-24 text-4xl"></div>;
+    return (
+        challenges ? (
+            <div className='p-2 flex flex-col gap-4'>
+                <div className='flex gap-2 w-full '>
+                    <div className='w-3/4'>
+                        <Challenge
+                            name='challenge name'
+                            description='Lorem ipsum dolor sit amet, consectetur adipisicing elit. Incidunt ex porro enim similique vero voluptatem voluptas sequi. Corporis ex, dolorem temporibus adipisci mollitia, corrupti assumenda, voluptate totam aut repellat voluptatibus!'
+                        />
+                    </div>
+                    <div className='w-1/4'>
+                        <ChallengeInfo items={items} />
+                    </div>
+                </div>
+                <div>carsoul</div>
+                <TournamentCard />
+            </div>
+        ) : (
+            <div className="flex flex-col w-full text-center py-24 gap-4" >
+                <Image src={imageEmpty} alt="" className='self-center' />
+                <p className='text-lg'>Currently there is now challenges come later to see what's new</p>
+            </div >
+        )
+    )
 }
+
