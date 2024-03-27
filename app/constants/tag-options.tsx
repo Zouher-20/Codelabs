6008684e1cf26568daca4407f653ad7dc4d8ce94
aymanm <099aymam@gmail.com>
2024-03-27 13:14:24 +0300

    Multi-select tag element's

---
+++ app/constants/tag-options.tsx
@@ -0,0 +1,11 @@
+import { MyOptionType } from "../@types/select";
+
+export const tagOptions: MyOptionType[] =
+    [
+        { value: "button", label: "button" },
+        { value: "input", label: "input" },
+        { value: "sideBar", label: "sideBar" },
+        { value: "navBar", label: "navBar" },
+        { value: "select", label: "select" },
+        { value: "card", label: "card" }
+    ];
