5c68d974eb60df6d40480e88e240a364b989d565
Zouhair Nasser <zouhair.nasser@workiom.com>
2024-04-25 17:09:27 +0300

    Fixed error in compile

--- app/api/(modules)/verified/(router)/register-otp/route.ts
+++
@@ -1,7 +0,0 @@
-import { NextResponse } from 'next/server';
-import VerifiedController from '../../controller/verified-controller';
-
-const controller = new VerifiedController();
-export async function POST(req: Request, res: NextResponse) {
-    return controller.registerOtp(req, res);
-}
