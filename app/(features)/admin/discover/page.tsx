df1f56a288a5766233ddcac546ece60ba8f1e2ab
Mhd <mhd.yazbek@yolo.clinic>
2024-05-02 15:04:02 +0300

    commit

--- app/(features)/admin/discover/page.tsx
+++ app/(features)/admin/discover/page.tsx
@@ -1,4 +1,5 @@
 'use client';
+import { getStatisticsAdmin } from '@/app/api/(modules)/admin/service/action';
 import { getlab } from '@/app/api/(modules)/admin/user-project/service/action';
 import { ManageState } from '@/app/components/page-state/state_manager';
 import { CustomToaster } from '@/app/components/toast/custom-toaster';
@@ -18,11 +19,19 @@ const Users = () => {
     const [loading, setLoading] = useState(true);
     const [error, setError] = useState<string | null>(null);
     const [searchWord, setSearchWord] = useState('');
+    const [statistics, setStatistics] = useState<{
+        blogs: number;
+        challenges: number;
+        classes: number;
+        labs: number;
+        users: number;
+    } | null>(null);
 
     useEffect(() => {
         var pageNumber = Number(params.get('id') ?? '1');
         updateCurrentPage(pageNumber);
         getUser({ newSearchWord: searchWord, page: pageNumber });
+        getServerStatistics();
     }, []);
     const getUser = async ({ newSearchWord, page }: { newSearchWord: string; page: number }) => {
         setLoading(true);
@@ -59,13 +68,50 @@ const Users = () => {
             setLoading(false);
         }
     };
+    const getServerStatistics = async () => {
+        try {
+            const res = await getStatisticsAdmin();
+            setStatistics(res);
+        } catch (error: any) {
+            toast.error(error.message);
+        }
+    };
     const onPageChange = ({ index }: { index: number }) => {
         updateCurrentPage(index);
         getUser({ newSearchWord: searchWord, page: index });
     };
     return (
         <div className="flex flex-col gap-2 p-6">
-            <AdminDiscoverStatisticsComponent icon="" text="Users" value={1256} />
+            {statistics ? (
+                <div className="flex flex-wrap justify-center gap-4">
+                    <AdminDiscoverStatisticsComponent
+                        icon="solar:user-bold"
+                        text="Users"
+                        value={statistics.users}
+                    />
+                    <AdminDiscoverStatisticsComponent
+                        icon="solar:test-tube-minimalistic-bold-duotone"
+                        text="Labs"
+                        value={statistics.labs}
+                    />
+                    <AdminDiscoverStatisticsComponent
+                        icon="solar:cup-star-bold-duotone"
+                        text="Challanges"
+                        value={statistics.challenges}
+                    />
+                    <AdminDiscoverStatisticsComponent
+                        icon="solar:square-academic-cap-bold-duotone"
+                        text="Classes"
+                        value={statistics.classes}
+                    />
+                    <AdminDiscoverStatisticsComponent
+                        icon="solar:text-field-bold-duotone"
+                        text="Blogs"
+                        value={statistics.blogs}
+                    />
+                </div>
+            ) : null}
+
             <div className="flex flex-col">
                 <DiscoverHeader
                     searchWord={searchWord}
