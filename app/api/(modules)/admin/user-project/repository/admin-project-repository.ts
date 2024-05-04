198551e053681f999dade3f463088dce0368ec8e
AbdAlrahman <abd233661@gmail.com>
2024-05-04 15:19:09 +0300

    get user project lab and get trending lab for users

--- app/api/(modules)/admin/user-project/repository/admin-project-repository.ts
+++ app/api/(modules)/admin/user-project/repository/admin-project-repository.ts
@@ -1,4 +1,5 @@
 import { db } from '@/app/api/core/db/db';
+import { Prisma } from '@prisma/client';
 
 class AdminProjectRepository {
     static async getUserProjects(payload: {
@@ -44,6 +45,97 @@ class AdminProjectRepository {
         const myProjects = await db.userProject.findMany({
             take: payload.pageSize,
             skip: skip,
+            orderBy: { createdAt: 'desc' },
+            include: {
+                user: true
+            },
+            where: {
+                ...args
+            }
+        });
+
+        const projectsWithCounts = await Promise.all(
+            myProjects.map(async project => {
+                const commentCount = await db.comment.count({
+                    where: { userprojectId: project.id }
+                });
+
+                const starCount = await db.star.count({
+                    where: { userprojectId: project.id }
+                });
+
+                return {
+                    ...project,
+                    commentCount,
+                    starCount
+                };
+            })
+        );
+
+        const totalCount = await db.userProject.count({
+            where: {
+                ...args
+            }
+        });
+
+        return {
+            projects: projectsWithCounts,
+            totalCount: totalCount
+        };
+    }
+
+    static async getTrendingUserProjectsLab(payload: {
+        page: number;
+        pageSize: number;
+        nameLab?: string;
+        tagName?: string;
+    }) {
+        const skip = (payload.page - 1) * payload.pageSize;
+        let args = {};
+
+        if (payload.nameLab && payload.tagName) {
+            args = {
+                AND: [
+                    {
+                        name: { contains: payload.nameLab }
+                    },
+                    {
+                        TagMorph: {
+                            tag: {
+                                tagename: { contains: payload.tagName }
+                            }
+                        }
+                    }
+                ]
+            };
+        } else if (payload.nameLab) {
+            args = {
+                name: { contains: payload.nameLab }
+            };
+        } else if (payload.tagName) {
+            args = {
+                TagMorph: {
+                    tag: {
+                        tagename: { contains: payload.tagName }
+                    }
+                }
+            };
+        }
+        const myProjects = await db.userProject.findMany({
+            take: payload.pageSize,
+            skip: skip,
+            orderBy: [
+                {
+                    Comment: {
+                        _count: Prisma.SortOrder.desc
+                    }
+                },
+                {
+                    Star: {
+                        _count: Prisma.SortOrder.desc
+                    }
+                }
+            ],
             include: {
                 user: true
             },
