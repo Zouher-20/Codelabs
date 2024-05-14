1138be2aeee247160c03c1f6804a294ff9ac66d6
abd23 <abd233661@gmail.com>
2024-05-14 05:37:08 -0700

    get my class & my class create by me

--- app/api/(modules)/class-room/services/action.ts
+++ app/api/(modules)/class-room/services/action.ts
@@ -1,9 +1,27 @@
+'use server';
+
 import { getSession } from '@/app/api/(modules)/auth/service/actions';
 import classRoomRepository from '@/app/api/(modules)/class-room/repository/class-room-repository';
-import { AddClassRoomInput } from '@/app/api/(modules)/class-room/types';
+import {
+    AddClassRoomInput,
+    getClassesByMeInput,
+    getMyClassesInput
+} from '@/app/api/(modules)/class-room/types';
 
 export const addClassRoom = async (payload: AddClassRoomInput) => {
     const session = await getSession();
     const userId = session?.id;
     return classRoomRepository.addClassRoom(payload, userId);
+};
+
+export const getClassCreateByMe = async (payload: getClassesByMeInput) => {
+    const session = await getSession();
+    const userId = session?.id;
+    return classRoomRepository.getClassCreateByMe(payload, userId);
+};
+
+export const getMyClass = async (payload: getMyClassesInput) => {
+    const session = await getSession();
+    const userId = session?.id;
+    return classRoomRepository.getMyClass(payload, userId);
 };
--- app/api/(modules)/class-room/types.ts
+++ app/api/(modules)/class-room/types.ts
@@ -3,3 +3,14 @@ export interface AddClassRoomInput {
     description?: string;
     type?: string;
 }
+
+export interface getClassesByMeInput {
+    page: number;
+    pageSize: number;
+    searchWord?: string;
+}
+export interface getMyClassesInput {
+    page: number;
+    pageSize: number;
+    searchWord?: string;
+}
