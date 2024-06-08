083e7cffb09275d79fbfc2d9f35177ac4cc61851
Mhd <mhd.yazbek@yolo.clinic>
2024-06-08 14:31:18 +0300

    comdsa

--- app/(features)/(main)/classes/students/room/page.tsx
+++ app/(features)/(main)/classes/students/room/page.tsx
@@ -1,6 +1,13 @@
 'use client';
 
-import { FeedbackType } from '@/app/@types/feedback';
+import { RoomType } from '@/app/@types/room';
+import { getClassRoomAndTeacherDetails } from '@/app/api/(modules)/class-room/services/action';
+import { EmptyState } from '@/app/components/page-state/empty';
+import { LoadingState } from '@/app/components/page-state/loading';
+import { ManageState } from '@/app/components/page-state/state_manager';
+import { useRouter, useSearchParams } from 'next/navigation';
+import { useEffect, useState } from 'react';
+import CodeLabContainer from '../../components/container';
 import ClassDescriptionComponent from '../../statistics/components/class-description';
 import FeedbackModal from '../../statistics/components/feedback-modal';
 import StatisticsContainer from '../../statistics/components/statistics_components';
@@ -8,38 +15,47 @@ import CloneLabComponent from './components/clone-lab-component';
 import { FeedbackComponent } from './components/feed-back';
 
 export default function ClassLabPage() {
-    var feedbacks: Array<FeedbackType> = [
-        {
-            id: '1',
-            feedback:
-                'Purus lorem dolor dolor euismod lorem facilisi amet arcu mi. Dui amet massa sociis volutpat viverra donec augue sit. Suscipit elementum eget rhoncus sed facilisis nisi. Orci facilisis at fermentum vitae pellentesque. Fermentum nisl aliquam rhoncus ipsum fames est augue.',
-            user: { email: 'alshalabi211@gmai.com', id: '1', name: 'majd' }
-        },
-        {
-            id: '2',
-            feedback:
-                'Purus lorem dolor dolor euismod lorem facilisi amet arcu mi. Dui amet massa sociis volutpat viverra donec augue sit. Suscipit elementum eget rhoncus sed facilisis nisi. Orci facilisis at fermentum vitae pellentesque. Fermentum nisl aliquam rhoncus ipsum fames est augue.',
-            user: { email: 'alshalabi211@gmai.com', id: '1', name: 'majd' }
-        },
-        {
-            id: '3',
-            feedback:
-                'Purus lorem dolor dolor euismod lorem facilisi amet arcu mi. Dui amet massa sociis volutpat viverra donec augue sit. Suscipit elementum eget rhoncus sed facilisis nisi. Orci facilisis at fermentum vitae pellentesque. Fermentum nisl aliquam rhoncus ipsum fames est augue.',
-            user: { email: 'alshalabi211@gmai.com', id: '2', name: 'majd2' }
-        },
-        {
-            id: '4',
-            feedback:
-                'Purus lorem dolor dolor euismod lorem facilisi amet arcu mi. Dui amet massa sociis volutpat viverra donec augue sit. Suscipit elementum eget rhoncus sed facilisis nisi. Orci facilisis at fermentum vitae pellentesque. Fermentum nisl aliquam rhoncus ipsum fames est augue.',
-            user: { email: 'alshalabi211@gmai.com', id: '1', name: 'majd' }
-        },
-        {
-            id: '5',
-            feedback:
-                'Purus lorem dolor dolor euismod lorem facilisi amet arcu mi. Dui amet massa sociis volutpat viverra donec augue sit. Suscipit elementum eget rhoncus sed facilisis nisi. Orci facilisis at fermentum vitae pellentesque. Fermentum nisl aliquam rhoncus ipsum fames est augue.',
-            user: { email: 'alshalabi211@gmai.com', id: '3', name: 'majd3' }
+    const currentParams = useSearchParams();
+    const route = useRouter();
+    useEffect(() => {
+        const id = currentParams.get('roomId') ?? '-1';
+        getRoomInfo({ id });
+    }, []);
+
+    const [roomLoading, setRoomLoading] = useState(true);
+    const [roomError, setRoomError] = useState(null);
+    const [roomInfo, setRoomInfo] = useState<RoomType | null>(null);
+
+    const getRoomInfo = async ({ id }: { id: string }) => {
+        setRoomLoading(true);
+        try {
+            const res = await getClassRoomAndTeacherDetails({ classRomId: id });
+            setRoomInfo({
+                id: id,
+                title: res.classDetails.name,
+                description: res.classDetails.description,
+                endAt: res.classDetails.endAt,
+                type: res.classDetails.type,
+                createdAt: res.classDetails.createdAt
+            });
+        } catch (e: any) {
+            setRoomError(e.message);
+        } finally {
+            setRoomLoading(false);
         }
-    ];
+    };
+
+    const calculateDurationPercentage = ({ start, end }: { start: Date; end: Date }) => {
+        const startTime = start.getTime();
+        const endTime = end.getTime();
+        const currentTime = new Date().getTime();
+
+        const totalDurationMs = endTime - startTime;
+        const remainingDurationMs = endTime - currentTime;
+        const remainingPercentage = (remainingDurationMs / totalDurationMs) * 100;
+
+        return Math.max(Math.min(Math.round(remainingPercentage), 100), 0); // Ensure the percentage is between 0 and 100
+    };
     const onFeedbackClicked = () => {
         if (document) {
             (document.getElementById('feedback-modal') as HTMLFormElement)?.showModal();
@@ -49,11 +65,37 @@ export default function ClassLabPage() {
     return (
         <div className="flex min-h-[550px] flex-col gap-2 p-3">
             <div className="flex w-full flex-wrap gap-2 md:w-1/4">
-                <StatisticsContainer
-                    color="#50FA7B"
-                    primaryText="Duration"
-                    anotherText="left"
-                    withAdd={false}
+                <ManageState
+                    loading={roomLoading}
+                    error={roomError}
+                    errorAndEmptyCallback={() => {
+                        const id = currentParams.get('roomId') ?? '-1';
+                    }}
+                    customEmptyPage={
+                        <CodeLabContainer>
+                            <EmptyState />
+                        </CodeLabContainer>
+                    }
+                    customLoadingPage={
+                        <CodeLabContainer>
+                            <LoadingState />
+                        </CodeLabContainer>
+                    }
+                    loadedState={
+                        <StatisticsContainer
+                            color="#E3E354"
+                            primaryText="Duration"
+                            anotherText="Left"
+                            series={[
+                                calculateDurationPercentage({
+                                    end: roomInfo?.endAt ?? new Date(),
+                                    start: roomInfo?.createdAt ?? new Date()
+                                })
+                            ]}
+                            withAdd={false}
+                        />
+                    }
+                    empty={false}
                 />
             </div>
             <ClassDescriptionComponent
@@ -66,7 +108,7 @@ export default function ClassLabPage() {
                 teacher={{ name: 'majd', id: '1', email: 'alshalabi211@gmai.com' }}
             />
             <div className="flex gap-2 max-md:flex-wrap">
-                <FeedbackComponent feedbacks={feedbacks} onClick={onFeedbackClicked} />
+                <FeedbackComponent feedbacks={[]} onClick={onFeedbackClicked} />
                 <CloneLabComponent buttonText="Clone To Start Coding" onButtonClick={() => {}} />
             </div>
             <FeedbackModal comments={[]} />
