462886f0ba6d760f0e514fcb23d6fc8c523ea0f2
Mhd <mhd.yazbek@yolo.clinic>
2024-06-20 15:02:06 +0300

    commit

--- app/(features)/(main)/classes/statistics/room/lab/page.tsx
+++ app/(features)/(main)/classes/statistics/room/lab/page.tsx
@@ -1,45 +1,11 @@
 'use client';
 
-import { FeedbackType } from '@/app/@types/feedback';
 import { useRouter, useSearchParams } from 'next/navigation';
 import CloneLabComponent from '../../../students/room/components/clone-lab-component';
 import { FeedbackComponent } from '../../../students/room/components/feed-back';
 import FeedbackModal from '../../components/feedback-modal';
 
 export default function ClassLabPage() {
-    const feedbacks: Array<FeedbackType> = [
-        {
-            id: 1,
-            feedback:
-                'Purus lorem dolor dolor euismod lorem facilisi amet arcu mi. Dui amet massa sociis volutpat viverra donec augue sit. Suscipit elementum eget rhoncus sed facilisis nisi. Orci facilisis at fermentum vitae pellentesque. Fermentum nisl aliquam rhoncus ipsum fames est augue.',
-            user: { id: '1', name: 'majd', email: 'alshalabi211@gmail.com' }
-        },
-        {
-            id: 2,
-            feedback:
-                'Purus lorem dolor dolor euismod lorem facilisi amet arcu mi. Dui amet massa sociis volutpat viverra donec augue sit. Suscipit elementum eget rhoncus sed facilisis nisi. Orci facilisis at fermentum vitae pellentesque. Fermentum nisl aliquam rhoncus ipsum fames est augue.',
-            user: { id: '2', name: 'majd', email: 'alshalabi211@gmail.com' }
-        },
-        {
-            id: 3,
-            feedback:
-                'Purus lorem dolor dolor euismod lorem facilisi amet arcu mi. Dui amet massa sociis volutpat viverra donec augue sit. Suscipit elementum eget rhoncus sed facilisis nisi. Orci facilisis at fermentum vitae pellentesque. Fermentum nisl aliquam rhoncus ipsum fames est augue.',
-            user: { id: '3', name: 'majd2', email: 'alshalabi211@gmail.com' }
-        },
-        {
-            id: 4,
-            feedback:
-                'Purus lorem dolor dolor euismod lorem facilisi amet arcu mi. Dui amet massa sociis volutpat viverra donec augue sit. Suscipit elementum eget rhoncus sed facilisis nisi. Orci facilisis at fermentum vitae pellentesque. Fermentum nisl aliquam rhoncus ipsum fames est augue.',
-            user: { id: '4', name: 'majd', email: 'alshalabi211@gmail.com' }
-        },
-        {
-            id: 5,
-            feedback:
-                'Purus lorem dolor dolor euismod lorem facilisi amet arcu mi. Dui amet massa sociis volutpat viverra donec augue sit. Suscipit elementum eget rhoncus sed facilisis nisi. Orci facilisis at fermentum vitae pellentesque. Fermentum nisl aliquam rhoncus ipsum fames est augue.',
-            user: { id: '5', name: 'majd3', email: 'alshalabi211@gmail.com' }
-        }
-    ];
-
     const currentParams = useSearchParams();
     const route = useRouter();
 
@@ -62,7 +28,7 @@ export default function ClassLabPage() {
     return (
         <div className="flex min-h-[550px] flex-col gap-2 p-3">
             <div className="flex gap-2 max-md:flex-wrap">
-                <FeedbackComponent feedbacks={feedbacks} onClick={onFeedbackClicked} />
+                <FeedbackComponent feedbacks={[]} onClick={onFeedbackClicked} />
                 <CloneLabComponent buttonText="view lab" onButtonClick={handleLabClick} />
             </div>
             <FeedbackModal comments={[]} />
