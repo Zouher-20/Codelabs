c98ab2a24c0a4534fee84522c5ee23af221d2f9d
abd23 <abd233661@gmail.com>
2024-06-22 04:06:16 -0700

    upload file

---
+++ app/api/(modules)/admin/template/(router)/image-upload/route.ts
@@ -0,0 +1,19 @@
+import { NextResponse } from 'next/server';
+import { uploadImage } from '../../services/action';
+
+export async function POST(req: Request) {
+    try {
+        const data = await req.formData();
+        const file: File | null = data.get('file') as unknown as File;
+        const response = await uploadImage({ file });
+        return NextResponse.json({
+            data: response
+        });
+
+    } catch (err) {
+        console.log('Error:', err);
+        return NextResponse.json({
+            data: "error while uploading"
+        });
+    }
+}
