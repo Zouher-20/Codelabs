a0048f7b49f7f4fa636f015e5cf2f8ec1cd4366e
abd23 <abd233661@gmail.com>
2024-06-22 05:20:41 -0700

    file json

--- app/api/(modules)/admin/template/repository/template-repository.ts
+++ app/api/(modules)/admin/template/repository/template-repository.ts
@@ -1,25 +1,49 @@
 import { db } from "@/app/api/core/db/db";
-import { mkdir, writeFile } from 'node:fs/promises';
+import { mkdir, writeFile, readFile } from 'node:fs/promises';
 import { join } from 'path';
+import { randomBytes } from 'crypto';
+
 class TemplateRepository {
     static async addTemplate(payload: { nameTemplate: string; imageTemplate: string }) {
+        const newLab = await db.lab.create({
+            data: {
+                jsonFile: "",
+            }
+        });
 
-        const existsingTemplate = await db.tamblate.findUnique({
+        const existingTemplate = await db.tamblate.findUnique({
             where: {
                 nameTemplate: payload.nameTemplate
             }
         });
-        if (existsingTemplate) {
-            throw new Error("please choose another name ");
+        if (existingTemplate) {
+            throw new Error("please choose another name");
         }
+        const defaultJsonPath = join(process.cwd(), 'public', 'lab', 'default.json');
+        const uploadDir = join(process.cwd(), 'public', 'lab');
+        const newJsonFilePath = join(uploadDir, `${payload.nameTemplate}.json`);
+        const defaultJsonContent = await readFile(defaultJsonPath, 'utf-8');
+        await mkdir(uploadDir, { recursive: true });
+        await writeFile(newJsonFilePath, defaultJsonContent);
         const newTemplate = await db.tamblate.create({
             data: {
                 imageTemplate: payload.imageTemplate,
-                nameTemplate: payload.nameTemplate
+                nameTemplate: payload.nameTemplate,
+                labId: newLab.id
             }
         });
+        await db.lab.update({
+            where: {
+                id: newLab.id
+            },
+            data: {
+                jsonFile: newJsonFilePath
+            }
+        });
+
         return newTemplate;
     }
+
 
     static async uploadImage(payload: { file: File }) {
         try {
@@ -27,11 +51,16 @@ class TemplateRepository {
             if (!file) {
                 throw new Error("No file uploaded");
             }
+
             const bytes = await file.arrayBuffer();
             const buffer = Buffer.from(bytes);
 
-            const uploadDir = join(process.cwd(), 'public', 'uploads');
-            const filePath = join(uploadDir, file.name);
+            const uploadDir = join(process.cwd(), 'public', 'uploads', "images");
+            const randomName = randomBytes(16).toString('hex');
+            const extension = file.name.split('.').pop();
+            const fileName = `${randomName}.${extension}`;
+
+            const filePath = join(uploadDir, fileName);
 
             await mkdir(uploadDir, { recursive: true });
 
@@ -39,7 +68,7 @@ class TemplateRepository {
 
             console.log('File received:', file);
 
-            const url = join('/uploads', file.name);
+            const url = join('/uploads', fileName);
 
             return url;
 
