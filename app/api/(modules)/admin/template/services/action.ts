4d769cc2a8c5d7bff51824e88ec777c1172ad49e
abd23 <abd233661@gmail.com>
2024-06-23 05:27:55 -0700

    add and delete and search and get template

--- app/api/(modules)/admin/template/services/action.ts
+++ app/api/(modules)/admin/template/services/action.ts
@@ -1,6 +1,6 @@
 import { ROLE } from "@prisma/client";
 import { getSession } from "../../../auth/service/actions";
-import { addTemplateInput, uploadTemplateImageInput } from "../../types";
+import { DeleteTemplateInput, addTemplateInput, getAllTemplateInput, uploadTemplateImageInput } from "../../types";
 import TemplateRepository from "../repository/template-repository";
 
 export const uploadImage = async (payload: uploadTemplateImageInput) => {
@@ -19,3 +19,20 @@ export const addTemplate = async (payload: addTemplateInput) => {
         throw new Error("you are dont admin");
     }
 };
+export const getAllTemplate = async (payload: getAllTemplateInput) => {
+
+    return TemplateRepository.getAllTemplate(payload);
+
+};
+
+export const deletTemplate = async (payload: DeleteTemplateInput) => {
+    const session = await getSession();
+    const role = session?.role;
+    if (role == ROLE.ADMIN) {
+        return TemplateRepository.deleteTemplate(payload);
+    }
+    else {
+        throw new Error("you are dont admin");
+    }
+};
+
