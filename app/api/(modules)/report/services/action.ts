6cc0f28085d31c8deabb60a49b32583d844bb93e
abd23 <abd233661@gmail.com>
2024-07-16 07:56:18 -0700

    sss

--- app/api/(modules)/report/services/action.ts
+++ app/api/(modules)/report/services/action.ts
@@ -1,5 +1,5 @@
 
-"user server";
+"use server";
 import { getSession } from '../../auth/service/actions';
 import ReportRepository from '../repository/report-repository';
 import { AddReportInput } from '../type';
